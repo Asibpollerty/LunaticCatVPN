a027e7cd344d6cced077e330406931d94b37b58a
Asibpollerty <aphexsibpollerty@gmail.com>
2026-03-24 13:22:32 +0700

    update keys

--- config.py
+++ config.py
@@ -8,48 +8,48 @@ ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "0").split(",")))
 DB_PATH = "lunaticcat.db"
 
 VPN_SERVERS = {
-    "🇨🇦 Канада": {
-        "flag": "🇨🇦",
-        "country": "Канада",
-        "city": "Canada",
-        "ping": "~80ms",
-        "load": "low",
-        "key": "ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpBUmd2R1p5d0ErZ2FjZ0dWMjZCdm11MDUrd1ptUlcvaitBZFUrWjhCdDQ0PQ==@79.127.200.169:990#CA"
-    },
     "🇺🇸 США 1": {
         "flag": "🇺🇸",
         "country": "США",
         "city": "New York",
         "ping": "~89ms",
-        "load": "medium",
-        "key": "ss://YWVzLTEyOC1nY206c2hhZG93c29ja3M=@156.146.38.170:443#US1"
+        "load": "low",
+        "key": "vmess://eyJ2IjoiMiIsInBzIjoiXHUyNmY4XHVmZTBmXHUyNmY4XHVmZTBmIiwiYWRkIjoic2VydmVyMy5iZWhsZXNodGJhbmVoLmNvbSIsInBvcnQiOiIyMDQwMSIsImlkIjoiNmEzYmNjMDgtOWM3Ny00YzAyLTg0NGItNGE2OTQzNGYyZmVhIiwiYWlkIjoiMCIsIm5ldCI6IndzIiwicGF0aCI6Ii8iLCJ0bHMiOiIifQ=="
     },
     "🇺🇸 США 2": {
         "flag": "🇺🇸",
         "country": "США",
         "city": "Los Angeles",
         "ping": "~95ms",
-        "load": "low",
-        "key": "ss://YWVzLTEyOC1nY206c2hhZG93c29ja3M=@156.146.38.167:443#US2"
-    },
-    "🇺🇸 США 3": {
-        "flag": "🇺🇸",
-        "country": "США",
-        "city": "Chicago",
-        "ping": "~92ms",
         "load": "medium",
-        "key": "ss://YWVzLTEyOC1nY206c2hhZG93c29ja3M=@37.19.198.236:443#US3"
+        "key": "ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpkNjEwNWJiZC1iZTBkLTQ1YjItODJhZC0zMWZkMTA3MWMxZDI@service.ouluyu n9803.com:20003"
     },
-    "🇺🇸 США 4": {
-        "flag": "🇺🇸",
-        "country": "США",
-        "city": "Dallas",
-        "ping": "~98ms",
-        "load": "high",
-        "key": "ss://YWVzLTEyOC1nY206c2hhZG93c29ja3M=@37.19.198.236:443#US4"
+    "🇨🇦 Канада": {
+        "flag": "🇨🇦",
+        "country": "Канада",
+        "city": "Toronto",
+        "ping": "~75ms",
+        "load": "low",
+        "key": "ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpkNjEwNWJiZC1iZTBkLTQ1YjItODJhZC0zMWZkMTA3MWMxZDI@service.ouluyu n9803.com:26667"
+    },
+    "🇯🇵 Япония": {
+        "flag": "🇯🇵",
+        "country": "Япония",
+        "city": "Tokyo",
+        "ping": "~145ms",
+        "load": "low",
+        "key": "vmess://eyJhZGQiOiJzZXJ2ZXIzMi5iZWhsZXNodGJhbmVoLmNvbSIsInBvcnQiOiI0NDMiLCJpZCI6IjRiYThiZWRkLTcyODUtNDcyYS1iYzE0LWZiOTFkYzZiZTRjOSIsImFpZCI6IjAiLCJuZXQiOiJ3cyIsInBhdGgiOiIvIiwidGxzIjoiIn0="
+    },
+    "🇩🇪 Германия": {
+        "flag": "🇩🇪",
+        "country": "Германия",
+        "city": "Frankfurt",
+        "ping": "~35ms",
+        "load": "low",
+        "key": "vmess://eyJhZGQiOiJzZXJ2ZXIzMS5iZWhsZXNodGJhbmVoLmNvbSIsInBvcnQiOiI0NDMiLCJpZCI6IjE4MjgzZDItZTk2OC00MmUxLTgwZDAtMTJmYTJmNWQzOGQ2IiwiYWlkIjoiMCIsIm5ldCI6IndzIiwicGF0aCI6Ii8iLCJ0bHMiOiIifQ=="
     }
 }
 
 FREE_KEYS_PER_DAY = 1
 KEY_DURATION_DAYS = 1
-CHANNEL_ID = "@LunaticCatVPN"
+CHANNEL_ID = "@VPNLunaticCat"
